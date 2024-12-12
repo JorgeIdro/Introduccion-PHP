202e4b092ad8cffdaee9b2b3662b534a59fdf5f3
JorgeIdro <jorgeidrovodw@gmail.com>
2024-12-12 17:52:20 -0500

    For Each con varios arrays

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor curvo 20"',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
-
+foreach ($productos as $producto) { ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre']; ?></p>
+        <p>Precio: <?php echo '$ ',$producto['precio']; ?> </p>
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible';?> </p> <!-- Operador condicional (ternario) -->
+    </li>
+    <?php
+    // echo "<li>";
+    // var_dump($producto);
+    // echo "</li>";
+}
 
 include 'includes/footer.php';
