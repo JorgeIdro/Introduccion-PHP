2748a6aa808599a34e4e62b7179aa7cf648f0c10
JorgeIdro <jorgeidrovodw@gmail.com>
2024-12-16 21:09:52 -0500

    json_encode y json_decode

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,38 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor curvo 20"',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
+echo '<pre>';
+var_dump($productos);
 
+echo '<br>';
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); // convertimos un array en string, el siguiente argumento es el tipo de conversion para que las tildes no tenga un error
+var_dump($json);
+
+echo '<br>';
+
+$json_array = json_decode($json); // Convertimos un string en array
+var_dump($json_array);
+echo '</pre>';
+
+// Util en caso de enviar el array y comunicar php con otras plataformas como javascript, react, vue.js
 
 
 include 'includes/footer.php';
